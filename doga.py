6a4b15b47d117caba85206409de68e929a349e19
Kardulecz Viktor <viktorkardulecz@gmail.com>
2022-10-06 15:49:59 +0200

    first commit

---
+++ doga.py
@@ -0,0 +1,6 @@
+#Háromszög területe
+
+mag=int(input("Kérem adja meg a magasságot.: "))
+old=int(input("Kérem adja meg az oldalak hosszát.: "))
+eredmeny=mag*old/2
+print("A háromszög területe",eredmeny)
